7c6051a7b1b8b669b09a6aca2f63c66665c65781
edogola4 <edogola4@gmail.com>
2024-10-24 12:22:22 +0300

    creating-DOM-nodes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// Remove the <main> element with the id main
+document.getElementById('main').remove();
+
+// create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Assign an id of victory to the new header
+newHeader.id = 'victory';
+
+// Set the text content of the new header
+newHeader.textContent = 'Brandon Ogola is the champion';
+
+// Append the new header to the body
+document.body.appendChild(newHeader);
